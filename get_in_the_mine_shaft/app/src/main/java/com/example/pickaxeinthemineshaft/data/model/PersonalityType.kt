6ec4a898ef568ff9255291d965c58ec1b920a82b
HudAli23 <huddaifah25@hotmail.com>
2026-02-13 00:48:48 +0000

    Delete get_in_the_mine_shaft directory

--- get_in_the_mine_shaft/app/src/main/java/com/example/pickaxeinthemineshaft/data/model/PersonalityType.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pickaxeinthemineshaft.data.model
-
-enum class PersonalityType {
-    FRIENDLY,
-    SERIOUS,
-    PLAYFUL,
-    MYSTERIOUS,
-    ENERGETIC
-}
--- get_in_the_mine_shaft/check_java.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Checking Java version...
-java -version
-echo.
-echo If you see Java version 21, you need to set JAVA_HOME to point to JDK 11
-echo Example: set JAVA_HOME="C:\Program Files\Java\jdk-11"
-echo.
-echo Current JAVA_HOME is: %JAVA_HOME%
